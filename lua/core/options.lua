30dd32c240e842206723345ce7321017375f6000
vineet-53 <vineetsinghnegi53@gmail.com>
2025-01-31 20:56:32 +0530

    added color scheme

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -39,5 +39,4 @@ vim.o.undofile = true -- Save undo history (default: false)
 vim.o.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience (default: 'menu,preview')
 vim.opt.shortmess:append("c") -- Don't give |ins-completion-menu| messages (default: does not include 'c')
 vim.opt.iskeyword:append("-") -- Hyphenated words recognized by searches (default: does not include '-')
-vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 vim.opt.clipboard = ""
--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,28 +1,115 @@
+local function color()
+	vim.g.nord_contrast = true
+	vim.g.nord_borders = false
+	vim.g.nord_disable_background = true
+	vim.g.nord_italic = false
+	vim.g.nord_uniform_diff_background = true
+	vim.g.nord_bold = false
+end
 return {
-	"shaunsingh/nord.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		-- Example config in lua
-		vim.g.nord_contrast = true
-		vim.g.nord_borders = false
-		vim.g.nord_disable_background = true
-		vim.g.nord_italic = false
-		vim.g.nord_uniform_diff_background = true
-		vim.g.nord_bold = false
+	{
+		"shaunsingh/nord.nvim",
+		enabled = false,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Example config in lua
+			-- Load the colorscheme
+			require("nord").set()
 
-		-- Load the colorscheme
-		require("nord").set()
+			color()
+			-- Toggle background transparency
+			local bg_transparent = true
 
-		-- Toggle background transparency
-		local bg_transparent = true
+			local toggle_transparency = function()
+				bg_transparent = not bg_transparent
+				vim.g.nord_disable_background = bg_transparent
+				vim.cmd([[colorscheme nord]])
+			end
 
-		local toggle_transparency = function()
-			bg_transparent = not bg_transparent
-			vim.g.nord_disable_background = bg_transparent
-			vim.cmd([[colorscheme nord]])
-		end
+			vim.keymap.set("n", "<leader>bg", toggle_transparency, { noremap = true, silent = true })
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		config = function()
+			color()
+			require("rose-pine").setup({
+				variant = "auto", -- auto, main, moon, or dawn
+				dark_variant = "main", -- main, moon, or dawn
+				dim_inactive_windows = false,
+				extend_background_behind_borders = true,
 
-		vim.keymap.set("n", "<leader>bg", toggle_transparency, { noremap = true, silent = true })
-	end,
+				enable = {
+					terminal = true,
+					legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+					migrations = true, -- Handle deprecated options automatically
+				},
+
+				styles = {
+					bold = true,
+					italic = true,
+					transparency = false,
+				},
+
+				groups = {
+					border = "muted",
+					link = "iris",
+					panel = "surface",
+
+					error = "love",
+					hint = "iris",
+					info = "foam",
+					note = "pine",
+					todo = "rose",
+					warn = "gold",
+
+					git_add = "foam",
+					git_change = "rose",
+					git_delete = "love",
+					git_dirty = "rose",
+					git_ignore = "muted",
+					git_merge = "iris",
+					git_rename = "pine",
+					git_stage = "iris",
+					git_text = "rose",
+					git_untracked = "subtle",
+
+					h1 = "iris",
+					h2 = "foam",
+					h3 = "rose",
+					h4 = "gold",
+					h5 = "pine",
+					h6 = "foam",
+				},
+
+				palette = {
+					-- Override the builtin palette per variant
+					-- moon = {
+					--     base = '#18191a',
+					--     overlay = '#363738',
+					-- },
+				},
+
+				highlight_groups = {
+					-- Comment = { fg = "foam" },
+					-- VertSplit = { fg = "muted", bg = "muted" },
+				},
+
+				before_highlight = function(group, highlight, palette)
+					-- Disable all undercurls
+					-- if highlight.undercurl then
+					--     highlight.undercurl = false
+					-- end
+					--
+					-- Change palette colour
+					-- if highlight.fg == palette.pine then
+					-- 	highlight.fg = palette.foam
+					-- end
+				end,
+			})
+
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
 }
